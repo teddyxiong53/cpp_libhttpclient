cad4422a370d4c0d3cebdc47798b21a35b9bcb09
teddyxiong53 <1073167306@qq.com>
2019-05-28 13:48:04 +0800

    打印post和get的参数。

--- webserver/app.js
+++ webserver/app.js
@@ -1,16 +1,18 @@
 var express = require("express")
 var app = express()
 
-var multipart = require('connect-multiparty');
-var multipartMiddleware = multipart();
+var bodyParser = require("body-parser")
 
-app.post('/', multipartMiddleware, function(req, res, next) {
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.post('/', function(req, res, next) {
     // 业务逻辑
     console.log(req.body);
     res.end("hello post")
 });
 app.get('/', function(req, res) {
     console.log("hello get")
+    console.log(req.query)
     res.end("hello get")
 })
 app.listen(3000)
